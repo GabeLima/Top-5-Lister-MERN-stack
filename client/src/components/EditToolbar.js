70482353639e19f58a2a0df7cb760f752be6828f
“Gabriello <gabriello.lima@stonybrook.edu>
2021-10-09 17:08:52 -0400

    Refreshing page mechanic works now.

--- client/src/components/EditToolbar.js
+++ client/src/components/EditToolbar.js
@@ -11,6 +11,32 @@ function EditToolbar() {
     const { store } = useContext(GlobalStoreContext);
     const history = useHistory();
 
+    let counter = 0;
+    function refreshPage(){
+        sessionStorage.setItem("reloading", "true");
+
+        console.log("Inside refresh page!");
+        if(counter === 0 && store.idNamePairs.length === 0 && history != null && history.location != null){
+            let pathName = history.location.pathname;
+            console.log(pathName);
+            if(pathName.includes("/top5list/")){
+                console.log("Loading the id name pairs after refresh!");
+                let id = pathName.substring("/top5list/".length);
+                console.log(id);
+                store.loadIdNamePairs();
+                store.setCurrentList(id);
+            }
+            counter = 1;
+        }
+    }
+    window.onload = function() {
+        var reloading = sessionStorage.getItem("reloading");
+        if(reloading !== true){
+            refreshPage();
+        }
+    }
+
+
     let enabledButtonClass = "top5-button";
     let disabledButtonClass = "top5-button-disabled";
     
@@ -21,9 +47,14 @@ function EditToolbar() {
         store.redo();
     }
     function handleClose() {
-        //console.log(history);
-        console.log(store.currentList);
-        if(store.currentList !== null){
+        // let pathName = history.location.pathname;
+        // console.log(history);
+        // console.log(store);
+        // console.log(store.currentList);
+        // console.log(store.currentList === null);
+        // console.log(store.listNameActive === false);
+        // console.log(store.currentList === null);
+        if(store.currentList !== null || (store.currentList === null && store.listNameActive === false && store.idNamePairs.length === 0)){
             history.push("/");
             store.closeCurrentList();
         }
